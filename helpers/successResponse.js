4b1fc0742d21be7e515ff881f43a1409ff50f408
Rolando Grethe Ojeda <rolandogretheojeda@gmail.com>
2023-03-20 16:38:00 -0300

    adding helper for success response and finish user model for db

---
+++ helpers/successResponse.js
@@ -0,0 +1,21 @@
+const endpointResponse = ({
+    res,
+    code = 200,
+    status = true,
+    message,
+    body,
+    options,
+}) => {
+    const response = {
+        status,
+        code,
+        message,
+        body,
+        options,
+    }
+    res.status(code).json(response)
+
+}
+
+module.exports = endpointResponse
+
---
+++ models/Users.js
@@ -0,0 +1,98 @@
+const mongoose = require('mongoose');
+const bcrypt = require("bcrypt")
+const deletedUsers = require("./deletedUsers")
+const errorHandler = require("../helpers/errorHandler")
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        minlength: [3, "name must have more than 3 characters"],
+        maxlength: [20, "name cant have more than 20 characters"],
+        required: true
+    },
+    role: {
+        type: String,
+        required: true,
+        default: "client"
+    },
+
+    email: {
+        type: String,
+        required: true,
+        validate: [function (email) {
+            var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return re.test(email)
+        }, "not a valid email adress"],
+        unique: [true, "email already exist"]
+    },
+    isLogin: {
+        type: Boolean,
+        default: false
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+
+//query middleware for passing the deleted document to the deletedUser collection, for saving the data that is erraise.
+
+userSchema.pre("deleteOne", async function (next) {
+
+
+    try {
+        const document = await this.model.findOne(this.getQuery());
+        const { name, email, password, role, createdAt } = document
+
+        deletedUsers.create({
+            name,
+            email,
+            password,
+            role,
+            createdAt
+        })
+
+        next()
+    }
+    catch {
+        const error = new errorHandler("cant soft delete the user", 500)
+        next(error)
+
+    }
+
+
+})
+
+// document middleware for hashing the password before saving it
+
+userSchema.pre("save", async function (next) {
+
+
+    try {
+        const hashPassword = await bcrypt.hash(this.password, 10)
+        this.password = hashPassword
+
+        next()
+    }
+    catch (err) {
+
+        const error = new errorHandler(err, 500)
+        next(error)
+
+    }
+
+
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
