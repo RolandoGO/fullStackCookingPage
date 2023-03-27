c2d589d312a57c18e49c78f68df728067eb7341b
Rolando Grethe Ojeda <rolandogretheojeda@gmail.com>
2023-03-26 23:48:44 -0300

    make the functionality for the create recepie controler

--- controlers/recepieControler.js
+++ controlers/recepieControler.js
@@ -12,7 +12,43 @@ const recepieControler = {
 
     },
 
-    createRecepie: () => {
+    createRecepie: async (req, res, next) => {
+
+        if (typeof req.body === typeof []) {
+            try {
+                await Recepie.insertMany(req.body)
+                successResponse({
+                    res,
+                    message: "recepies created"
+                })
+
+            }
+            catch {
+
+                const error = new ErrorHandler("cant create recepies", 500)
+                next(error)
+                return
+            }
+        }
+        else {
+            try {
+                await Recepie.create(req.body)
+                successResponse({
+                    res,
+                    message: "recepie created"
+                })
+
+            }
+            catch {
+
+                const error = new ErrorHandler("cant create recepie", 500)
+                next(error)
+                return
+            }
+
+        }
+
+
 
     },
 
