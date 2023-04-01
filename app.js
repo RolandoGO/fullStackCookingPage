7111711d9fee6ef3afd5deb6a0a175793b464fc5
Rolando Grethe Ojeda <rolandogretheojeda@gmail.com>
2023-03-31 21:13:13 -0300

    adding css to the ejs files in views folder

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs")
+app.use(express.static(__dirname + '/public'));
+
 
 // ROUTES
 app.use("/", indexRouter)
